0fbf63d896f86dea70e13f156be1a40de6d8ae39
Brianleft28 <contactobrianleft@gmail.com>
2024-01-30 14:43:37 -0300

    estudiando useState

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,27 @@
 import './StyleSheets/Navbar.css'
+import { useState } from 'react'
 
 export function Navbar() {
   return (
     <>
       <nav className='navbar'>
         <span className='navbar__brand'>
-          <h1>Brian Benegas</h1>
+          <h1>Portfolio | Brian Benegas</h1>
         </span>
+
         <section className='navbar__lista'>
           <ul>
             <li className='navbar__item-list'>
-              <a href='#'></a>
+              <a href='#'>About Me</a>
             </li>
             <li className='navbar__item-list'>
-              <a href='#'></a>
+              <a href='#'>Projects</a>
             </li>
             <li className='navbar__item-list'>
-              <a href='#'></a>
+              <a href='#'>Skills</a>
             </li>
             <li className='navbar__item-list'>
-              <a href='#'></a>
+              <a href='#'>Contact</a>
             </li>
           </ul>
         </section>
