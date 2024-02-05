b4f2850966c888a88e1d3333f3b7c325ca2f1e38
Brianleft28 <contactobrianleft@gmail.com>
2024-02-05 12:51:07 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { AboutMe } from './components/AboutMe'
 import { Contact } from './components/Contact'
 import { Projects } from './components/Projects'
 import { Skills } from './components/Skills'
-import { Navbar } from './components/Navbar'
+import { NavBar } from './components/Navbar'
 import { Education } from './components/Education'
 import { Experience } from './components/Experience'
 import { Footer } from './components/Footer'
@@ -15,7 +15,7 @@ function App() {
   return (
     <>
       <div>
-        <Navbar />
+        <NavBar />
         <Header />
         <Footer />
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,87 @@
-import './StyleSheets/Navbar.css'
-import { useState } from 'react'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import { useState, useEffect } from 'react'
 
-export function Navbar() {
+export const NavBar = () => {
+  const [activeLink, setActiveLink] = useState('home')
+  const [scrolled, setScrolled] = useState(false)
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true)
+      } else {
+        setScrolled(false)
+      }
+    }
+
+    window.addEventListener('scroll', onScroll)
+
+    return () => {
+      window.removeEventListener('scroll', onScroll)
+    }
+  }, [])
+
   return (
     <>
-      <nav className='navbar'>
-        <span className='navbar__brand'>
-          <h1>Portfolio | Brian Benegas</h1>
-        </span>
-
-        <section className='navbar__lista'>
-          <ul>
-            <li className='navbar__item-list'>
-              <a href='#'>About Me</a>
-            </li>
-            <li className='navbar__item-list'>
-              <a href='#'>Projects</a>
-            </li>
-            <li className='navbar__item-list'>
-              <a href='#'>Skills</a>
-            </li>
-            <li className='navbar__item-list'>
-              <a href='#'>Contact</a>
-            </li>
-          </ul>
-        </section>
-      </nav>
+      <Navbar expand='lg'>
+        <Container>
+          <Navbar.Brand href='#home'>
+            <img src={''} alt='Logo' />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav'>
+            <span className='navbar-toggler-icon'></span>
+          </Navbar.Toggle>
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='me-auto'>
+              <Nav.Link
+                href='#home'
+                className={
+                  activeLink === 'home' ? 'active navbar-link' : 'navbar-link'
+                }
+                onClick={() => onUpdateActiveLink('home')}>
+                Home
+              </Nav.Link>
+              <Nav.Link
+                href='#skills'
+                className={
+                  activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'
+                }
+                onClick={() => onUpdateActiveLink('skills')}>
+                Skills
+              </Nav.Link>
+              <Nav.Link
+                href='#projects'
+                className={
+                  activeLink === 'projects'
+                    ? 'active navbar-link'
+                    : 'navbar-link'
+                }
+                onClick={() => onUpdateActiveLink('projects')}>
+                Projects
+              </Nav.Link>
+            </Nav>
+            <span className='navbar-text'>
+              <div className='social-icon'>
+                <a href='#'>
+                  <img src={''} alt='' />
+                </a>
+                <a href='#'>
+                  <img src={''} alt='' />
+                </a>
+                <a href='#'>
+                  <img src={''} alt='' />
+                </a>
+              </div>
+              <button className='vvd' onClick={() => console.log('connect')}>
+                {' '}
+                <span>Let's Connect</span>
+              </button>
+            </span>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   )
 }
