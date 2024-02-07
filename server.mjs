a5b67fe2103ff9140c12d214a7d7a09722b4cca5
Brianleft28 <contactobrianleft@gmail.com>
2024-02-07 13:29:55 -0300

    sv- config

---
+++ server.mjs
@@ -0,0 +1,60 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import express from 'express'
+import cors from 'cors'
+import nodemailer from 'nodemailer'
+
+const router = express.Router()
+
+// Resto del código...
+
+// server used to send send emails
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use('/', router)
+app.listen(5000, () => console.log('Server Running'))
+console.log(process.env.EMAIL_USER)
+console.log(process.env.EMAIL_PASS)
+
+const contactEmail = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    host: 'smtp.gmail.com',
+    port: 587,
+    user: 'bbdevcontact@gmail.com',
+    pass: 'pvki hoit tdou trbn'
+  }
+})
+
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error)
+  } else {
+    console.log('Ready to Send')
+  }
+})
+
+router.post('/contact', (req, res) => {
+  const name = req.body.firstName + req.body.lastName
+  const email = req.body.email
+  const message = req.body.message
+  const phone = req.body.phone
+  const mail = {
+    from: name,
+    to: 'contactobrianleft@gmail.com',
+    subject: 'Contact Form - Portfolio',
+    html: `<p>Name: ${name}</p>
+           <p>Email: ${email}</p>
+           <p>Phone: ${phone}</p>
+           <p>Message: ${message}</p>`
+  }
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json(error)
+    } else {
+      res.json({ code: 200, status: 'Message Sent' })
+    }
+  })
+})
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Container, Col, Row } from 'react-bootstrap'
 import contactImg from '../assets/img/contact-img.svg'
+
 export const Contact = () => {
   const formInitialDetails = {
     firstName: '',
@@ -11,8 +12,51 @@ export const Contact = () => {
   }
 
   const [formDetails, setFormDetails] = useState(formInitialDetails)
+
   const [buttonText, setButtonText] = useState('Send')
   const [status, setStatus] = useState({})
+
+  const onFormUpdate = (category, value) => {
+    setFormDetails({
+      ...formDetails,
+      [category]: value
+    })
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setButtonText('Sending...')
+
+    try {
+      let response = await fetch('http://localhost:5000/contact', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(formDetails)
+      })
+
+      let result = await response.json()
+
+      setButtonText('Send')
+      setFormDetails(formInitialDetails)
+
+      if (result.code === 200) {
+        setStatus({ success: true, message: 'Message sent successfully' })
+      } else {
+        setStatus({
+          success: true,
+          message: 'Something went wrong, please try again later.'
+        })
+      }
+    } catch (error) {
+      // Handle any errors that occur during the fetch and parsing of JSON
+      console.error('Error during fetch:', error)
+      setStatus({
+        success: false,
+        message: 'An error occurred. Please try again later.'
+      })
+    }
+  }
 
   /* Renderizado */
   return (
@@ -20,7 +64,66 @@ export const Contact = () => {
       <Container>
         <Row className='align-items-center'>
           <Col md={6}>
-            <img src={contactImg} alt='Imagen de Contacto' />
+            <img src={contactImg} alt='Contact us' />
+          </Col>
+          <Col md={6}>
+            <h2>Get In Touch</h2>
+            <form onSubmit={handleSubmit}>
+              <Row>
+                <Col sm={6} className='px-1'>
+                  <input
+                    type='text'
+                    value={formDetails.firstName}
+                    placeholder='First Name'
+                    onChange={(e) => onFormUpdate('firstName', e.target.value)}
+                  />
+                </Col>
+                <Col sm={6} className='px-1'>
+                  <input
+                    type='text'
+                    value={formDetails.lastName}
+                    placeholder='Last Name'
+                    onChange={(e) => onFormUpdate('lastName', e.target.value)}
+                  />
+                </Col>
+                <Col sm={12} className='px-1'>
+                  <input
+                    type='email'
+                    value={formDetails.email}
+                    placeholder='Email Address'
+                    onChange={(e) => onFormUpdate('email', e.target.value)}
+                  />
+                </Col>
+                <Col sm={12} className='px-1'>
+                  <input
+                    type='tel'
+                    value={formDetails.phone}
+                    placeholder='Phone No.'
+                    onChange={(e) => onFormUpdate('phone', e.target.value)}
+                  />
+                </Col>
+                <Col>
+                  <textarea
+                    rows='6'
+                    value={formDetails.message}
+                    placeholder='Message'
+                    onChange={(e) =>
+                      onFormUpdate('message', e.target.value)
+                    }></textarea>
+                  <button type='submit'>
+                    <span>{buttonText}</span>
+                  </button>
+                </Col>
+                {status.message && (
+                  <Col>
+                    <p
+                      className={status.sucess === false ? 'danger' : 'sucess'}>
+                      {status.message}
+                    </p>
+                  </Col>
+                )}
+              </Row>
+            </form>
           </Col>
         </Row>
       </Container>
